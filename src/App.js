162243dde54504b7a9244f1ff598f503e9782ed3
malgorzatamaz <malgorzatamaz@gmail.com>
2019-01-28 19:40:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import TodoList from './components/TodoList';
 import store from './redux/store';
 import { Provider } from 'react-redux'
 
-// dpienczykowski@gmail.com
-// przerobić na reduxa, dodać testy, zrobić brancha
-
 class App extends Component {
     render() {
         return (
